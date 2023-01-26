e26fc0979ff0160b2f3d9ddd24dbe5d37d8c9bfe
Damish-N <damishnisal100@gmail.com>
2023-01-26 07:39:25 +0530

    #1-Features:DASHBOARD CHANGES

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -6,14 +6,12 @@ function Dashboard(props) {
     return (
         <div className={Styles.dashboardContainer}>
             <Grid className={Styles.item} container direction={"row"} justifyContent={"space-around"} spacing={2}>
-                <Grid onClick={() => {
-                    console.log("Hello")
-                }} className={Styles.itemContainer} xs={10} md={4}>
+                <Grid className={Styles.itemContainer} xs={10} md={4}>
                     <Paper className={Styles.item_paper}>
                         <Typography variant={"h5"} className={Styles.textColor}>
                             25
                         </Typography>
-                        <Typography variant={"h6"} className={Styles.textColor}>
+                        <Typography variant={"subtitle2"} className={Styles.secondTextColor}>
                             PLAYERS
                         </Typography>
                     </Paper>
@@ -21,20 +19,20 @@ function Dashboard(props) {
                 <Grid className={Styles.itemContainer} xs={10} md={4}>
                     <Paper className={Styles.item_paper}>
                         <Typography variant={"h5"} className={Styles.textColor}>
-                            15
+                            25
                         </Typography>
-                        <Typography variant={"h6"} className={Styles.textColor}>
-                            MATCHES
+                        <Typography variant={"subtitle2"} className={Styles.secondTextColor}>
+                            SEASONED PLAYED MATCHES
                         </Typography>
                     </Paper>
                 </Grid>
                 <Grid className={Styles.itemContainer} xs={10} md={4}>
                     <Paper className={Styles.item_paper}>
                         <Typography variant={"h5"} className={Styles.textColor}>
-                            Rs:25,502
+                            25,500
                         </Typography>
-                        <Typography variant={"h6"} className={Styles.textColor}>
-                            CASH IN HAND
+                        <Typography variant={"subtitle2"} className={Styles.secondTextColor}>
+                            CASH IN HAND(Rs)
                         </Typography>
                     </Paper>
                 </Grid>
