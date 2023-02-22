b1495fa05e5f48985924656168e3d0a04a4dc9b6
Damish-N <damishnisal100@gmail.com>
2023-02-23 01:19:27 +0530

    #1-Features:Added Pie Chart and Create Matches Detail Pages

--- src/pages/Dashboard/Matches/Matches.js
+++ src/pages/Dashboard/Matches/Matches.js
@@ -22,6 +22,30 @@ import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
 import ManIcon from "@mui/icons-material/Man";
 import image from "../../../asserts/logo.png";
 import { SportsSoccer } from "@material-ui/icons";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+export const data = {
+  labels: ["Win", "LOSS", "DRAWS"],
+  datasets: [
+    {
+      label: "# of Number",
+      data: [12, 19, 3],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+      ],
+      borderWidth: 0.1,
+    },
+  ],
+};
 
 function Matches(props) {
   const defaultProps = {
@@ -372,6 +396,47 @@ function Matches(props) {
       <Grid xs={12} md={4} item>
         <Paper className={Styles.paperPadding}>
           <h1 style={{ color: "#ffffff" }}>Match Stats</h1>
+        </Paper>
+        <Paper sx={{ padding: "15px" }}>
+          <Pie data={data} />
+          <Paper className={Styles.status}>
+            <Chip
+              color={"primary"}
+              sx={{
+                color: "#ffffff",
+                padding: "0.2rem",
+              }}
+              size="small"
+              label="Played-40"
+            />
+            <Chip
+              color={"primary"}
+              sx={{
+                color: "#ffffff",
+                padding: "0.2rem",
+              }}
+              size="small"
+              label="Won-12"
+            />
+            <Chip
+              color={"primary"}
+              sx={{
+                color: "#ffffff",
+                padding: "0.2rem",
+              }}
+              size="small"
+              label="Lost-12"
+            />
+            <Chip
+              color={"primary"}
+              sx={{
+                color: "#ffffff",
+                padding: "0.2rem",
+              }}
+              size="small"
+              label="Draw-12"
+            />
+          </Paper>
         </Paper>
       </Grid>
     </Grid>
